59a28f1faf4c80947f88160359e37914e14e167a
Dina Ramadan <dinaramadan02@gmail.com>
2025-05-28 20:29:34 +0200

    Repository Docstrings

--- repository.py
+++ repository.py
@@ -7,11 +7,29 @@ from constants import config_file
 ##marche
 
 def get_config():
+    """
+    Load and return the project configuration from the config file.
+    :return: the serialized data
+    """
+
     config_full_path = os.path.join(os.getcwd(), config_file)
     return get_serialized_data(config_full_path)
 
 class Repository:
+    """
+    A class used to load data either from a SQLite database or CSV files,
+    depending on the project settings.
+    """
+
     def __init__(self, config: dict, database_path: str = None, output_path: str = None) -> None:
+        """
+        Initialize the Repository with config and data source paths.
+
+        :param config: dictionary with settings and file names
+        :param database_path: path to the SQLite database
+        :param output_path: path to the CSV files folder
+        """
+
         self.config = config
         self.database_path = database_path
         self.output_path = output_path
@@ -19,6 +37,12 @@ class Repository:
         self.largest_companies = None
 
     def get_data(self) -> None:
+        """
+        Load data from the source defined in the configuration.
+        Either reads from a SQLite database or from CSV files.
+        :return: none
+        """
+
         if self.config['etl_main_parameters']['to_sqlite']:
             engine = create_engine(f'sqlite:///{self.database_path}', echo = True)
             self.merged_data = pd.read_sql_table(self.config['files_sql']['merged_table'], engine)
@@ -46,6 +70,3 @@ if __name__ == '__main__':
     print(repo.merged_data.head())
 
     print(repo.largest_companies.head())
-
-
-
