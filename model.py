e2afaff737683cc8a9ede7f162e0a79169c16854
loaneib <loaneibrahimm@gmail.com>
2025-05-28 17:38:13 +0200

    on m'oblige a commit

--- model.py
+++ model.py
@@ -21,20 +21,22 @@ class Model:
         self.col_merged = self.config['rename_merged_table']
 
         self.col_gdp_merged = self.col_merged['GDP (USD Trillions)']
-        self.col_revenue_merged = self.col_merged['Mean Net Income in (USD Millions)']
         self.col_tax_rate_merged = self.col_merged['Corporate Tax Rate (%)']
         self.col_country_merged = self.col_merged['Headquarters']
         self.col_inflation = self.col_merged['Inflation Rate (%)']
         self.col_interest = self.col_merged['Interest Rate (%)']
-        self.col_mean_revenue_merged = self.config['rename_merged_table']['Mean Revenue in (USD Million)']
+        self.col_mean_revenue_merged = self.col_merged['Mean Revenue in (USD Million)']
+        self.col_net_income_merged = self.col_merged['Mean Net Income in (USD Millions)']
+        self.col_total_asset_merged = self.col_merged['Mean Total Asset in (USD Millions)']
 
         self.col = self.config['columns']['largest_companies']
 
         self.col_revenue = self.col['Revenue in (USD Million)']
         self.col_net_income = self.col['Net Income in (USD Millions)']
         self.col_industry = self.col['Industry']
-        self.col_total_asset = self.config['columns']['largest_companies']['Total Assest in (USD Millions)']
-        self.col_company = self.config['columns']['largest_companies']['Company']
+        self.col_total_asset = self.col['Total Assest in (USD Millions)']
+        self.col_company = self.col['Company']
+        self.col_country = self.col['Headquarters']
 
     def compute(self):
         df = self.repo.merged_data.copy()
@@ -59,17 +61,10 @@ class Model:
         print(biggest_sector.head())
         return biggest_sector
 
-    # def get_tax_burden(self):
-    #     df = self.repo.merged_data.copy()
-    #
-    #     df['Tax Burden (%)'] = (df[self.col_tax_rate_merged] * df[self.col_revenue_merged] / df[self.col_gdp_merged] * 1000000) * 100
-    #
-    #     return df[[self.col_country_merged, 'Tax Burden (%)']]
-
     def get_revenue_to_gdp(self):
         df = self.repo.merged_data.copy()
 
-        df['Revenue to GDP (%)'] = df[self.col_revenue_merged]/(df[self.col_gdp_merged] * 1000000) * 100
+        df['Revenue to GDP (%)'] = df[self.col_mean_revenue_merged]/(df[self.col_gdp_merged] * 1000000) * 100
 
         return df[[self.col_country_merged, 'Revenue to GDP (%)']]
 
@@ -80,36 +75,55 @@ class Model:
 
         return df[[self.col_country_merged, 'Real Interest Rate (%)']]
 
-    def get_new_table(self): #changer le nom je manque d'inspi
-        df1 = self.get_tax_burden()
-        df2 = self.get_revenue_to_gdp()
-        df3 = self.get_real_interest_rate()
-
-        df = df1.merge(df2, on = self.col_country_merged)
-        df = df.merge(df3, on = self.col_country_merged)
-
-        return df
-
     def get_asset_efficiency(self):
         df = self.repo.largest_companies.copy()
+
         df['Asset Efficiency'] = df[self.col_revenue] / df[self.col_total_asset]
+
         return df[[self.col_company, 'Asset Efficiency']]
 
     def get_average_contribution_to_public_finances(self):
         df = self.repo.merged_data.copy()
-        df['Tax Contribution (% of GDP)'] = ((df[self.col_tax_rate_merged] * df[self.col_mean_revenue_merged]) / df[
-            self.col_gdp_merged]) * 100
+        ### *100000 je pense
+        df['Average Contribution to Public Finances (% of GDP)'] = ((df[self.col_tax_rate_merged] * df[self.col_mean_revenue_merged]) / df[
+                                                self.col_gdp_merged]) * 100
+
         return df[[self.col_country_merged, 'Average Contribution to Public Finances (% of GDP)']]
 
-    def get_return_on_assets(self):  # ROA
-        df = self.repo.largest_companies.copy()
+    def get_return_on_assets(self):
+        df = self.repo.merged_data.copy()
+
         df['Return on Assets'] = (df[self.col_net_income] / df[self.col_total_asset]) * 100
-        return df[[self.col_company, 'Return on Assets']]
+
+        return df[[self.col_country, 'Return on Assets']]
 
     def get_average_ROA_per_country(self):
-        df = self.repo.largest_companies.copy()
-        df['Average ROA'] = df[[self.col_company, 'Return on Assets']].groupby('Country').mean()
-        return df[self.col_country_merged, 'Average ROA']
+        df = self.repo.merged_data.copy()
+
+        df['Average ROA'] = (df[self.col_net_income_merged] / df[self.col_total_asset_merged]) * 100
+
+        return df[[self.col_country_merged, 'Average ROA']]
+
+    def get_new_table(self):  #changer le nom je manque d'inspi
+        #df1 = self.get_tax_burden()
+        df2 = self.get_revenue_to_gdp()
+        df3 = self.get_real_interest_rate()
+        df4 = self.get_average_contribution_to_public_finances()
+        df5 = self.get_average_ROA_per_country()
+
+        print("df2 countries:", set(df2[self.col_country_merged]))
+        print("df3 countries:", set(df3[self.col_country_merged]))
+        print("df4 countries:", set(df4[self.col_country_merged]))
+        print("df5 countries:", set(df5[self.col_country_merged]))
+
+        #df = df1.merge(df2, on = self.col_country_merged)
+        df = df2.merge(df3, on = self.col_country_merged)
+        df = df.merge(df4, on = self.col_country_merged)
+        df = df.merge(df5, on = self.col_country_merged)
+
+        return df
+
+
 
 
 if __name__ == '__main__':
