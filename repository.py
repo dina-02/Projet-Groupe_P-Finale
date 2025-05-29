4f75c405828bb83931d4ab94039ff9a13d6aa7b5
loaneib <loaneibrahimm@gmail.com>
2025-05-29 02:30:37 +0200

    qjout export sql dans model

--- repository.py
+++ repository.py
@@ -1,8 +1,9 @@
 import os.path
 import pandas as pd
+
+from constants import config_file
 from sqlalchemy import create_engine
 from helpers import get_serialized_data
-from constants import config_file
 
 ##marche
 
@@ -21,7 +22,7 @@ class Repository:
     depending on the project settings.
     """
 
-    def __init__(self, config: dict, database_path: str = None, output_path: str = None) -> None:
+    def __init__(self, config: dict, output_path: str = None) -> None:
         """
         Initialize the Repository with config and data source paths.
 
@@ -31,7 +32,6 @@ class Repository:
         """
 
         self.config = config
-        self.database_path = database_path
         self.output_path = output_path
         self.merged_data = None
         self.largest_companies = None
@@ -39,31 +39,21 @@ class Repository:
     def get_data(self) -> None:
         """
         Load data from the source defined in the configuration.
-        Either reads from a SQLite database or from CSV files.
+        Reads a CSV file.
         :return: none
         """
 
-        if self.config['etl_main_parameters']['to_sqlite']:
-            engine = create_engine(f'sqlite:///{self.database_path}', echo = True)
-            self.merged_data = pd.read_sql_table(self.config['files_sql']['merged_table'], engine)
-            self.largest_companies = pd.read_sql(self.config['files_sql']['source_largest_companies'], engine)
+        merged_file = os.path.join(self.output_path, self.config['files_csv']['merged_table'])
+        largest_file = os.path.join(self.output_path, self.config['files_csv']['source_largest_companies'])
 
-        elif self.config['etl_main_parameters']['to_csv']:
-                merged_file = os.path.join(self.output_path, self.config['files_csv']['merged_table'])
-                largest_file = os.path.join(self.output_path, self.config['files_csv']['source_largest_companies'])
-
-                self.merged_data = pd.read_csv(merged_file, sep = ',')
-                self.largest_companies = pd.read_csv(largest_file, sep = ',')
+        self.merged_data = pd.read_csv(merged_file, sep=',')
+        self.largest_companies = pd.read_csv(largest_file,sep =',')
 
 if __name__ == '__main__':
-    import os
-    from helpers import get_serialized_data
-    from constants import config_file
-
     config_path = os.path.join(os.getcwd(), config_file)
     config = get_serialized_data(config_path)
 
-    repo = Repository(config=config, output_path = 'output', database_path = 'output/output.sqlite')
+    repo = Repository(config=config, output_path='output')
 
     repo.get_data()
 
